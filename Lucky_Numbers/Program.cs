bd5f25e28aa4d698ba85c4dbae0bbd678668a527
monicawp <monicawperezf@gmail.com>
2017-10-08 10:18:41 -0400

    FINAL LUCKY NUMBERS

--- Lucky_Numbers/Program.cs
+++ Lucky_Numbers/Program.cs
@@ -17,10 +17,10 @@ namespace Lucky_Numbers
             do
             {
                 //Lucky Numbers Part 1
-
+                //CHECK VARIABLES TO MAKE SURE THEY ARE CORRECT camelCase OR PascalCase
 
                 Console.WriteLine("Hello and welcome to the Lucky Numbers Lottery Game!");
-                Console.WriteLine("Please enter the starting number for the first number in your guessing range");
+                Console.WriteLine("Please enter the first number in your guessing range");
                 int firstNumber = int.Parse(Console.ReadLine());
 
                 Console.WriteLine("Great, now enter the end number in your guessing range");
@@ -30,7 +30,10 @@ namespace Lucky_Numbers
                 Console.WriteLine("Now, you must guess 6 random numbers within the range that you picked");
 
 
-                //i = iterator 
+                //i = iterator- initializes the loop (can be labled anything 'i' 'j' 'k' etc)
+                //continue - pauses the current iteration and skips it based on the information given.
+                // in this case if the number is out of range it will ask the user to give a
+                //valid number
                 int[] guessedNumbers = new int[6];
                 for (int i = 0; i < guessedNumbers.Length; i++)
                 {
@@ -56,7 +59,7 @@ namespace Lucky_Numbers
                 double jackpot = 1000;
 
                 Console.WriteLine("Now... I am going to guess 6 random numbers within the range you chose.");
-                Console.WriteLine("If you guessed all 6 correctly, you could win " + jackpot);
+                Console.WriteLine("If you guessed all 6 correctly, you could win $" + jackpot);
                 Console.WriteLine("You will get a portion of the jackpot based on how many numbers you guess");
                 Console.WriteLine("correctly");
 
@@ -71,6 +74,8 @@ namespace Lucky_Numbers
                     Console.WriteLine("Lucky number: " + computerNumbers[number]);
                 }
 
+
+                // Part 3
                 // 'Contains' - if computerNumbers CONTAINS guessed numbers than it will count the correct numbers by one(++)
 
                 int correctGuess = 0;
@@ -85,43 +90,25 @@ namespace Lucky_Numbers
                 }
 
                 Console.WriteLine("You guessed " + correctGuess + " correct numbers!!");
-
+                //I googled the math rounding part and failed at it at least 4 times before i got it right
                 moneyWon = correctGuess * (jackpot / 6);
-                Console.WriteLine("You have won $ " + moneyWon);
+                Console.WriteLine("You have won $ " + Math.Round(moneyWon, 2,MidpointRounding.AwayFromZero)+ "!!!");
                 Console.WriteLine("Would you like to play again? YES/NO");
                 rePlay = Console.ReadLine().ToUpper();
             }
 
             while (rePlay == "YES");
+            // if the user answers yes to the rePlay, since the do statement is outside the initial code
+            // it will replay the entire game 
 
+            // if statement must be outside also
             if (rePlay == "NO")
             {
                 Console.WriteLine("Thanks for playing!"); 
             }
 
 
-                       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+             
         }
     }
 }
